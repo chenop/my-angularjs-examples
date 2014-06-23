4048adca226a5caba5ae554e82a7106d2d53f847
chenop <selavi99>
2014-06-23 21:13:10 +0300

    add userName greeting

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,6 +2,7 @@ var myApp = angular.module('myApp')
 
     .controller('WelcomeController', function ($scope) {
         $scope.greeting = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
+        $scope.userName = 'Chen';
     })
     .controller('TableController', function ($scope, $modal, dataService) {
         $scope.users = dataService.fetchTableData();
